68662692bb3c343ccacf2fbfa768984c68d847cd
Bay Raitt <67295292+bayraitt@users.noreply.github.com>
2021-02-09 15:02:00 -0800

    updated

--- ProcessFbxFolder.py
+++ ProcessFbxFolder.py
@@ -1,238 +1,190 @@
 bl_info = {
-        'name': 'Process FBX Files',
-        'author': 'Bay Raitt',
-        'version': (0, 1),
-        'blender': (2, 91, 0),
-        "description": "Import all FBX Folder and Export",
-        'category': 'Import-Export',
-        'location': 'File > Import',
-        'wiki_url': ''}
+    'name': 'ProcessFbxFolder',
+    'author': 'Bay Raitt',
+    'version': (0, 1),
+    'blender': (2, 91, 0),
+    "description": "Import all FBX Folder and Export",
+    'category': 'Import-Export',
+    'location': 'File > Import',
+    'wiki_url': ''}
 
 import bpy
 from bpy_extras.io_utils import ExportHelper
 from bpy_extras.io_utils import ImportHelper
 import os.path
-import bpy, os
 from bpy.props import *
-
-
+import subprocess
 import os
 import warnings
+from bpy.props import (
+    StringProperty,
+    BoolProperty,
+    EnumProperty,
+    FloatProperty,
+    IntProperty,
+    CollectionProperty,
+)
+
 
 # ImportHelper mixin class uses this
 filename_ext = ".fbx"
 
-filter_glob : StringProperty(
-        default="*.fbx",
-        options={'HIDDEN'},
-        )
+filter_glob: StringProperty(
+    default="*.fbx",
+    options={'HIDDEN'},
+)
 
-class BR_OT_process_fbx_folder(bpy.types.Operator, ImportHelper):
-        """Process Multiple Obj Files at Once"""
-        bl_idname = 'import_scene.process_fbx_folder'
-        bl_label = 'Process all fbx files in a folder and export'
-        bl_options = {'PRESET', 'UNDO'}
-        bl_description = "process all fbx files in a folder"
+class U_OT_process_fbx_folder(bpy.types.Operator, ImportHelper):
+    """Process Multiple FBX Files in a folder"""
+    bl_idname = 'import_scene.process_fbx_folder'
+    bl_label = 'Choose Mocap .fbx files'
+    bl_options = {'PRESET', 'UNDO'}
+    bl_description = "process all fbx files in a folder"
+
+    filepath = bpy.props.StringProperty(name="file path", description="Process folder")
+    files: CollectionProperty(type=bpy.types.PropertyGroup)
 
 
 
-        # Selected files
-        files : CollectionProperty(type=bpy.types.PropertyGroup)
+    def draw(self, context):
+        scene = bpy.context.scene
+        layout = self.layout
+        row = layout.row(align=True)
+        box: layout.box()
+        row: box.row()
+        row.prop(self, "trim_end_bones")
+        layout.prop(self, "keep_anim_scale")
+        layout.prop(self, "keep_anim_loc")
 
-        # List of operator properties, the attributes will be assigned
-        # to the class instance from the operator settings before calling.
-        ngons_setting : BoolProperty(
-                name="NGons",
-                description="Import faces with more than 4 verts as ngons",
-                default=True,
-                )
-        edges_setting : BoolProperty(
-                name="Lines",
-                description="Import lines and faces with 2 verts as edge",
-                default=True,
-                )
-        smooth_groups_setting : BoolProperty(
-                name="Smooth Groups",
-                description="Surround smooth groups by sharp edges",
-                default=True,
-                )
+    def execute(self, context):
 
-        split_objects_setting : BoolProperty(
-                name="Object",
-                description="Import OBJ Objects into Blender Objects",
-                default=True,
-                )
-        split_groups_setting : BoolProperty(
-                name="Group",
-                description="Import OBJ Groups into Blender Objects",
-                default=True,
-                )
+        trim_end_bones = False
+        keep_anim_scale = False
+        keep_anim_loc = False
+        scene = bpy.context.scene
+        objects = bpy.context.selected_objects
+        if objects is not None:
+            for obj in objects:
+                starting_mode = bpy.context.object.mode
+                if "OBJECT" not in starting_mode:
+                    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+                    bpy.ops.object.select_all(action='DESELECT')
 
-        groups_as_vgroups_setting : BoolProperty(
-                name="Poly Groups",
-                description="Import OBJ groups as vertex groups",
-                default=False,
-                )
+        # get the folder
+        folder = (os.path.dirname(self.filepath))
+        # iterate through the selected files
+        for i in self.files:
+            # generate full path to file
+            path_to_file = (os.path.join(folder, i.name))
 
-        image_search_setting : BoolProperty(
-                name="Image Search",
-                description="Search subdirs for any associated images "
-                            "(Warning, may be slow)",
-                default=True,
-                )
+            # call obj operator and assign ui values
+            bpy.ops.import_scene.fbx(filepath=path_to_file,
+                                     filter_glob='*.fbx',
+                                     ui_tab='MAIN',
+                                     use_manual_orientation=False,
+                                     global_scale=1.0,
+                                     bake_space_transform=False,
+                                     use_custom_normals=True,
+                                     use_image_search=True,
+                                     use_alpha_decals=False,
+                                     decal_offset=0.0,
+                                     use_anim=True,
+                                     anim_offset=1.0,
+                                     use_subsurf=False,
+                                     use_custom_props=True,
+                                     use_custom_props_enum_as_string=True,
+                                     ignore_leaf_bones=trim_end_bones,
+                                     force_connect_children=False,
+                                     automatic_bone_orientation=False,
+                                     primary_bone_axis='Y',
+                                     secondary_bone_axis='X',
+                                     use_prepost_rot=True,
+                                     axis_forward='-Z',
+                                     axis_up='Y')
 
-        split_mode_setting : EnumProperty(
-                name="Split",
-                items=(('ON', "Split", "Split geometry, omits unused verts"),
-                       ('OFF', "Keep Vert Order", "Keep vertex order from file"),
-                       ),
-                default='OFF',
-                )
+            # do stuff to the mesh here
+            imported_armature = ""
+            imported_objects = bpy.context.selected_objects
+            for ob in imported_objects:
+                bpy.ops.object.select_all(action='DESELECT')
+                ob.select_set(state=True)
+                bpy.context.view_layer.objects.active = ob
 
-        clamp_size_setting : FloatProperty(
-                name="Clamp Size",
-                description="Clamp bounds under this value (zero to disable)",
-                min=0.0, max=1000.0,
-                soft_min=0.0, soft_max=1000.0,
-                default=0.0,
-                )
-        axis_forward_setting : EnumProperty(
-                name="Forward",
-                items=(('X', "X Forward", ""),
-                       ('Y', "Y Forward", ""),
-                       ('Z', "Z Forward", ""),
-                       ('-X', "-X Forward", ""),
-                       ('-Y', "-Y Forward", ""),
-                       ('-Z', "-Z Forward", ""),
-                       ),
-                default='-Z',
-                )
+                # Check if object is a Mesh
+                # if ob.type == 'MESH':
 
-        axis_up_setting : EnumProperty(
-                name="Up",
-                items=(('X', "X Up", ""),
-                       ('Y', "Y Up", ""),
-                       ('Z', "Z Up", ""),
-                       ('-X', "-X Up", ""),
-                       ('-Y', "-Y Up", ""),
-                       ('-Z', "-Z Up", ""),
-                       ),
-                default='Y',
-                )
+                # Delete everything but armature
+                if ob.type != 'ARMATURE':
+                    bpy.ops.object.delete(use_global=False)
+                else:
+                    imported_armature = ob
 
-        def draw(self, context):
-            layout = self.layout
+            bpy.ops.object.select_all(action='DESELECT')
+            imported_armature.select_set(state=True)
+            bpy.context.view_layer.objects.active = imported_armature
 
-            row = layout.row(align=True)
-            row.prop(self, "ngons_setting")
-            row.prop(self, "edges_setting")
+            # clear off scale and position keyframes
+            root_bones = [b for b in imported_armature.data.bones if not b.parent]
+            if not keep_anim_scale or not keep_anim_loc:
+                # bpy.ops.object.mode_set(mode='POSE')
+                # bpy.ops.object.select_all(action='DESELECT')
+                # bpy.ops.pose.select_all(action='DESELECT')
 
-            layout.prop(self, "smooth_groups_setting")
+                # imported_armature.select = True
+                bpy.ops.object.mode_set(mode='POSE')
+                bpy.ops.pose.select_all(action='DESELECT')
+                for pb in imported_armature.pose.bones:
+                    if pb not in root_bones:
+                        imported_armature.data.bones[pb.name].select = True
+                        if not keep_anim_scale:
+                            bpy.ops.pose.scale_clear()
+                        if not keep_anim_loc:
+                            bpy.ops.pose.loc_clear()
+            bpy.ops.object.mode_set(mode='OBJECT')
 
-            box : layout.box()
-            row : box.row()
-            row.prop(self, "split_mode_setting", expand=True)
-
-            row : box.row()
-            if self.split_mode_setting == 'ON':
-                row.label(text="Split by:")
-                row.prop(self, "split_objects_setting")
-                row.prop(self, "split_groups_setting")
+            # check if actions is empty
+            if bpy.data.actions:
+                action_list = [action.frame_range for action in bpy.data.actions]
+                keys = (sorted(set([item for sublist in action_list for item in sublist])))
+                scene.frame_start = keys[0]
+                scene.frame_end = keys[-1]
             else:
-                row.prop(self, "groups_as_vgroups_setting")
+                print("no actions")
 
-            row.prop(self, "clamp_size_setting")
-            layout.prop(self, "axis_forward_setting")
-            layout.prop(self, "axis_up_setting")
+            # print("=======DEBUG: " + scene.name)
+            raise KeyboardInterrupt()
 
-            layout.prop(self, "image_search_setting")
+            # export the armature
+            basefilename = os.path.splitext(i.name)[0]
+            tmp_path_to_file = (os.path.join(folder, basefilename))
+            path_to_export_file = (tmp_path_to_file + "_processed.fbx")
+            bpy.ops.export_scene.fbx(filepath=path_to_export_file, use_selection=True)
 
-        def execute(self, context):
+            # delete the mesh
+            bpy.ops.object.delete(use_global=False)
+            cmd = ("explorer " + folder)
+            subprocess.Popen(cmd)
+        return {'FINISHED'}
 
-            # get the folder
-            folder = (os.path.dirname(self.filepath))
 
-            # iterate through the selected files
-            for i in self.files:
+class U_MT_BatchSubMenu(bpy.types.Menu):
+    bl_idname = 'U_MT_BatchSubMenu'
+    bl_label = 'Batch'
 
-                # generate full path to file
-                path_to_file = (os.path.join(folder, i.name))
-
-                # call obj operator and assign ui values                  
-                bpy.ops.import_scene.fbx(filepath = path_to_file,
-                                    axis_forward = self.axis_forward_setting,
-                                    axis_up = self.axis_up_setting, 
-                                    use_edges = self.edges_setting,
-                                    use_smooth_groups = self.smooth_groups_setting, 
-                                    use_split_objects = self.split_objects_setting,
-                                    use_split_groups = self.split_groups_setting,
-                                    use_groups_as_vgroups = self.groups_as_vgroups_setting,
-                                    use_image_search = self.image_search_setting,
-                                    split_mode = self.split_mode_setting,
-                                    global_clight_size = self.clamp_size_setting)
-                                    
-#  utils for batch processing kitbash stamps.       
-#                bpy.context.scene.objects.active = bpy.context.selected_objects[0]
-#                bpy.ops.object.modifier_add(type='SUBSURF')
-#                bpy.context.object.modifiers["Subsurf"].levels = 2
-#                bpy.ops.object.modifier_add(type='DECIMATE')
-#                bpy.context.object.modifiers["Decimate.001"].ratio = 0.5
-#                bpy.ops.object.modifier_add(type='DECIMATE')
-#                bpy.context.object.modifiers["Decimate.001"].decimate_type = 'DISSOLVE'
-#                bpy.context.object.modifiers["Decimate.001"].delimit = {'SEAM', 'SHARP'}
-#                bpy.context.object.modifiers["Decimate.001"].angle_limit = 0.0523599
-#                bpy.ops.object.modifier_add(type='TRIANGULATE')
-#                bpy.context.object.modifiers["Triangulate"].quad_method = 'BEAUTY'
-#                # bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Decimate")
-#                # bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Decimate.001")
-#                # bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Triangulate")
-#                bpy.ops.object.mode_set(mode='EDIT')
-#                bpy.ops.mesh.select_all(action = 'DESELECT')
-#                bpy.ops.mesh.select_all(action='TOGGLE')
-#                bpy.ops.mesh.tris_convert_to_quads()
-#                bpy.ops.mesh.faces_shade_smooth()
-#                bpy.ops.mesh.mark_sharp(clear=True)
-#                bpy.ops.mesh.mark_sharp(clear=True, use_verts=True)
-#                #if not bpy.context.object.data.uv_layers:
-#                bpy.ops.uv.smart_project(island_margin=0.01 , user_area_weight=0.75)
-#                bpy.ops.object.mode_set(mode='OBJECT')
-#                bpy.context.object.data.use_auto_smooth = True
-#                bpy.context.object.data.auto_smooth_angle = 0.575959
-
-                #process the mesh
-                imported_objects = bpy.context.selected_objects
-
-                # for ob in imported_objects:
-                #     ob.select_set(state=True)
-                #     bpy.context.view_layer.objects.active = ob
-                # bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-                # bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='VERT')
-                # bpy.ops.mesh.normals_make_consistent(inside=False)
-                # bpy.ops.mesh.remove_doubles()
-
-                #export the mesh
-                basefilename = os.path.splitext(i.name)[0]
-                tmp_path_to_file = (os.path.join(folder, basefilename))
-                path_to_export_file = (tmp_path_to_file + "_processed.fbx")
-            
-
-                bpy.ops.export_scene.fbx(   filepath = path_to_export_file, use_selection   =   True )
-
-                bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-                # delete the mesh
-                bpy.ops.object.delete(use_global=False)
-
-            return {'FINISHED'}
+    def draw(self, context):
+        layout = self.layout
+        self.layout.operator(U_OT_process_fbx_folder.bl_idname)
 
 
 def menu_import_draw(self, context):
-    self.layout.operator(BR_OT_process_fbx_folder.bl_idname)
+    layout = self.layout
+    layout.menu(U_MT_BatchSubMenu.bl_idname)
 
 
 classes = (
-    BR_OT_process_fbx_folder,
+    U_MT_BatchSubMenu,
+    U_OT_process_fbx_folder,
 )
-
 
 
 def register():
@@ -242,17 +194,16 @@ def register():
     bpy.types.TOPBAR_MT_file_import.append(menu_import_draw)
 
 
-                
-
 def unregister():
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
-            
+
     bpy.types.TOPBAR_MT_file_import.remove(menu_import_draw)
 
+
 if __name__ != "__main__":
-    bpy.types.VIEW3D_MT_view.remove(menu_import_draw)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_import_draw)
 
 if __name__ == "__main__":
     try:
